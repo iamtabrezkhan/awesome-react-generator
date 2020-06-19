84c5c52b184e3e134069265f3caa6de301756ea4
Shadab <shadab14meb346@gmail.com>
2020-06-19 09:32:11 +0530

    Add: test cases for feature rccp

--- test/cli.js
+++ test/cli.js
@@ -248,6 +248,117 @@ describe("component command", () => {
     });
   });
   // ========================================
+  describe(`rg c rccp ${componentName}`, () => {
+    afterEach(() => {
+      support.removeFolder(tempDir, pascalName);
+    });
+    // ======================================
+    it("should create correct number of files/folders", () => {
+      const nodeProcess = spawnSync("node", [rg, "c", "rccp", componentName], {
+        cwd: tempDir,
+      });
+      const tempContents = support.getDirContents(tempDir);
+      assert.equal(tempContents.length, 1);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      assert.equal(componentContents.length, 2);
+    });
+    it("should create correct name of files/folders", () => {
+      const nodeProcess = spawnSync("node", [rg, "c", "rccp", componentName], {
+        cwd: tempDir,
+      });
+      const tempContents = support.getDirContents(tempDir);
+      assert.deepEqual(tempContents, [pascalName]);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      expect(componentContents).to.deep.equalInAnyOrder([
+        componentFileName,
+        cssFileNameNormal,
+      ]);
+    });
+  });
+  // ========================================
+  describe(`rg c rccp ${componentName} --cssType modular`, () => {
+    afterEach(() => {
+      support.removeFolder(tempDir, pascalName);
+    });
+    it("should create correct number of files/folders", () => {
+      const nodeProcess = spawnSync(
+        "node",
+        [rg, "c", "rccp", componentName, "--cssType", "modular"],
+        {
+          cwd: tempDir,
+        }
+      );
+      const tempContents = support.getDirContents(tempDir);
+      assert.equal(tempContents.length, 1);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      assert.equal(componentContents.length, 2);
+    });
+    it("should create correct name of files/folders", () => {
+      const nodeProcess = spawnSync(
+        "node",
+        [rg, "c", "rccp", componentName, "--cssType", "modular"],
+        {
+          cwd: tempDir,
+        }
+      );
+      const tempContents = support.getDirContents(tempDir);
+      assert.deepEqual(tempContents, [pascalName]);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      expect(componentContents).to.deep.equalInAnyOrder([
+        componentFileName,
+        cssFileNameModular,
+      ]);
+    });
+  });
+  // ========================================
+  describe(`rg c rccp ${componentName} --test`, () => {
+    afterEach(() => {
+      support.removeFolder(tempDir, pascalName);
+    });
+    it("should create correct number of files/folders", () => {
+      const nodeProcess = spawnSync(
+        "node",
+        [rg, "c", "rccp", componentName, "--test"],
+        {
+          cwd: tempDir,
+        }
+      );
+      const tempContents = support.getDirContents(tempDir);
+      assert.equal(tempContents.length, 1);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      assert.equal(componentContents.length, 3);
+    });
+    it("should create correct name of files/folders", () => {
+      const nodeProcess = spawnSync(
+        "node",
+        [rg, "c", "rccp", componentName, "--test"],
+        {
+          cwd: tempDir,
+        }
+      );
+      const tempContents = support.getDirContents(tempDir);
+      assert.deepEqual(tempContents, [pascalName]);
+      const componentContents = support.getDirContents(
+        `${tempDir}/${pascalName}`
+      );
+      expect(componentContents).to.deep.equalInAnyOrder([
+        componentFileName,
+        cssFileNameNormal,
+        testFileName,
+      ]);
+    });
+  });
+  // ========================================
   describe(`rg c rfcp ${componentName}`, () => {
     afterEach(() => {
       support.removeFolder(tempDir, pascalName);
