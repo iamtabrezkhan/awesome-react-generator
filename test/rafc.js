19f300efd0014d595add8b8698b538e3936f28cd
Tabrez Khan <khan.t.29@gmail.com>
2020-07-01 18:59:28 +0530

    Update: travis file, fixing error while testing

--- test/rafc.js
+++ test/rafc.js
@@ -13,11 +13,10 @@ chai.use(deepEqualInAnyOrder);
 
 const tempDir = "test/rafc";
 
-before("create temp folder", () => {
-  utils.createFolder("test", "rafc");
-});
-
 describe("component command", () => {
+  before("create temp folder", () => {
+    utils.createFolder("test", "rafc");
+  });
   // ========================================
   const componentName = "hello-world";
   const pascalName = utils.kebabCaseToPascalCase(componentName);
@@ -299,8 +298,7 @@ describe("component command", () => {
       ]);
     });
   });
-});
-
-after("remove temp folder", () => {
-  support.removeFolder("test", "rafc");
+  after("remove temp folder", () => {
+    support.removeFolder("test", "rafc");
+  });
 });
