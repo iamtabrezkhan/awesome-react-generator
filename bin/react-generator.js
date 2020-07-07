f23c9fe27fcf696596f157b81beb8dade514b469
Shadab <shadab14meb346@gmail.com>
2020-07-07 18:22:30 +0530

    Feat:generate private route

--- bin/react-generator.js
+++ bin/react-generator.js
@@ -4,6 +4,7 @@ const yargs = require("yargs");
 yargs
   .usage("Usage: $0 <command> [options]")
   .command(require("../lib/commands/component"))
+  .command(require("../lib/commands/route"))
   .alias("h", "help")
   .demandCommand()
   .recommendCommands()
